f8b2655afef72622dca3736335d4b4a03f457261
mr-matthias1829 <matthiasdebee35@gmail.com>
2025-11-18 13:15:40 +0100

    INITIAL V2

--- p2.2-Digital-Twin/main.js
+++ p2.2-Digital-Twin/main.js
@@ -81,9 +81,127 @@ function setup() {
         },
     });
 
-    createModel("Cesium_Man.glb", latlonFromXY(220,70), 0);
+    createModel("Cesium_Man.glb", latlonFromXY(220, 70), 0);
 
-    createModel("strange_building.glb", latlonFromXY(240,70), 0);
+    createModel("strange_building.glb", latlonFromXY(240, 70), 0);
+
+    setupInputActions();
+}
+
+function createPoint(worldPosition) {
+    const point = viewer.entities.add({
+        position: worldPosition,
+        point: {
+            color: Cesium.Color.BLUE,
+            pixelSize: 5,
+            heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
+        },
+    });
+    return point;
+}
+
+let drawingMode = "polygon"; //Deze kun je aanpassen als je een GUI-element hiervoor maakt.
+
+function drawShape(positionData) {
+    let shape;
+    if (drawingMode === "line") {
+        shape = viewer.entities.add({
+            polyline: {
+                positions: positionData,
+                clampToGround: true,
+                width: 3,
+            },
+        });
+    } else if (drawingMode === "polygon") {
+        shape = viewer.entities.add({
+            polygon: {
+                hierarchy: positionData,
+                material: new Cesium.ColorMaterialProperty(
+                    Cesium.Color.RED.withAlpha(0.7),
+                ),
+            },
+        });
+    }
+    return shape;
+}
+
+function setupInputActions() {
+    viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(
+        Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK,
+    );
+
+    //Deze variabelen worden binnen deze functie gedeclareerd. Dit is vreemd, want je zou zeggen dat
+    //wanneer de functie voorbij is, de variabelen uit het geheugen worden verwijderd. Dit is in dit
+    //geval niet zo, omdat de variabelen worden gebruikt in de inline functies hieronder. Daardoor
+    //blijven ze bestaan. Op zich is dit handig, omdat de variabelen nu niet vanuit de globale
+    //scope bereikbaar zijn. Wel moet je letten op leesbaarheid. Soms is het handiger om de variabelen
+    //wel globaal te zetten.
+    let activeShapePoints = [];
+    let activeShape;
+    let floatingPoint;
+
+    const handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+
+    handler.setInputAction(function (event) {
+        // We use `viewer.scene.globe.pick here instead of `viewer.camera.pickEllipsoid` so that
+        // we get the correct point when mousing over terrain.
+        const ray = viewer.camera.getPickRay(event.position);
+        const earthPosition = viewer.scene.globe.pick(ray, viewer.scene);
+        // `earthPosition` will be undefined if our mouse is not over the globe.
+        if (Cesium.defined(earthPosition)) {
+            if (activeShapePoints.length === 0) {
+                floatingPoint = createPoint(earthPosition);
+                activeShapePoints.push(earthPosition);
+                const dynamicPositions = new Cesium.CallbackProperty(function () {
+                    if (drawingMode === "polygon") {
+                        return new Cesium.PolygonHierarchy(activeShapePoints);
+                    }
+                    return activeShapePoints;
+                }, false);
+                activeShape = drawShape(dynamicPositions);
+            }
+            activeShapePoints.push(earthPosition);
+            createPoint(earthPosition);
+        }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+    // Set an action for when the left mouse button is clicked while holding the CTRL key
+    handler.setInputAction(function (event) {
+        var pickedObject = viewer.scene.pick(event.position); // For hover effect
+        if (Cesium.defined(pickedObject)) {
+            const entity = viewer.entities.getById(pickedObject.id.id);
+            // entity.polygon.material.color = Cesium.Color.YELLOW;
+            create3DObject(entity, 20);
+            console.log(entity);
+            // Optionally, highlight the polygon or show any other UI feedback
+        }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK, Cesium.KeyboardEventModifier.CTRL);
+
+    handler.setInputAction(function (event) {
+        if (Cesium.defined(floatingPoint)) {
+            const ray = viewer.camera.getPickRay(event.endPosition);
+            const newPosition = viewer.scene.globe.pick(ray, viewer.scene);
+            if (Cesium.defined(newPosition)) {
+                floatingPoint.position.setValue(newPosition);
+                activeShapePoints.pop();
+                activeShapePoints.push(newPosition);
+            }
+        }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+    // Redraw the shape so it's not dynamic and remove the dynamic shape.
+    function terminateShape() {
+        activeShapePoints.pop();
+        drawShape(activeShapePoints);
+        viewer.entities.remove(floatingPoint);
+        viewer.entities.remove(activeShape);
+        floatingPoint = undefined;
+        activeShape = undefined;
+        activeShapePoints = [];
+    }
+    handler.setInputAction(function (event) {
+        terminateShape();
+    }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
 }
 
 // x = verplaatsing in meters noord (+) / zuid (-)
@@ -120,13 +238,28 @@ var _box = 1;
 //simpel via Blender. Zie de volgende tutorial: https://www.youtube.com/watch?v=mURA2g1rOSc
 function createBox(x, y, width, depth, height, rotation, color) {
     const cords = latlonFromXY(x, y);
+    return createBoxLatLon(cords, width, depth, height, rotation, color);
+}
 
+function createBoxLatLon(cords, width, depth, height, rotation, color) {
     return viewer.entities.add({
         name: "Box_" + _box++,
         position: Cesium.Cartesian3.fromDegrees(cords.lat, cords.lon, height / 2.0),
         box: {
             dimensions: new Cesium.Cartesian3(width, depth, height),
             material: color
+        }
+    });
+}
+
+function createBoxXYZ(position, width, depth, height, rotation, color) {
+    return viewer.entities.add({
+        name: "Box_" + _box++,
+        position: position,
+        box: {
+            dimensions: new Cesium.Cartesian3(width, depth, height),
+            material: color,
+            heightReference: Cesium.HeightReference.CLAMP_TO_GROUND //IMPORTANT
         }
     });
 }
@@ -189,4 +322,47 @@ function createModel(url, position, height) {
         },
     });
     viewer.trackedEntity = entity;
+}
+
+// Function to convert Cartesian coordinates to latitude and longitude
+function cartesianToLatLon(cartesianPosition) {
+    const cartographic = Cesium.Cartographic.fromCartesian(cartesianPosition);
+    // Convert radians to degrees
+    const lon = cartographic.longitude;
+    const lat = cartographic.latitude;
+
+    return { lat, lon };
+}
+
+// Define grid size
+const gridSize = 1.1; // Adjust this to your desired grid size
+
+// Function to snap coordinates to the grid
+function snapToGrid(position) {
+    const snappedX = Math.round(position.x / gridSize) * gridSize;
+    const snappedZ = Math.round(position.z / gridSize) * gridSize;
+
+    return new Cesium.Cartesian3(snappedX, position.y, snappedZ);
+}
+
+function handleMouseClick(event) {
+    const mousePosition = new Cesium.Cartesian2(event.clientX, event.clientY);
+    //const ray = viewer.camera.getPickRay(mousePosition);
+    const hitPosition = viewer.scene.pickPosition(mousePosition);
+
+    // Check if the ray intersects the globe
+    if (hitPosition) {
+        var snappedPosition = snapToGrid(hitPosition);
+
+        createBoxXYZ(snappedPosition, 1, 1, 1, 0, Cesium.Color.RED);
+    }
+}
+
+function create3DObject(basePolygon, height) {
+    if (basePolygon.polygon.extrudedHeight == undefined) {
+        basePolygon.polygon.extrudedHeight = height;
+    }
+
+    //Deze extrudedHeight komt heel goed van pas
+    basePolygon.polygon.extrudedHeight *= 1.5;
 }
